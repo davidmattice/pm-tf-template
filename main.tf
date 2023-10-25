8bcc234b46eaba6cdb07b46b74c325179850b90c
Dave Mattice <david.mattice@hotmail.com>
2023-10-25 09:05:06 -0400

    use git module

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 
 
 module "template" {
-  source          = "./modules/template"
+  source          = "git::https://github.com/davidmattice/pve-tfm-template?ref=initial"
+  providers = {
+    proxmox = proxmox
+  }
+  #source          = "./modules/template"
   pve_endpoint    = var.pve_endpoint
   pve_user        = var.pve_user
   pve_template_id = var.pve_template_id
